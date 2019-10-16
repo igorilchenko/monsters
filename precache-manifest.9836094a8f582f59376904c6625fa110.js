7f0784ba1b148d715a2095200344d69184fbdbb1
Vladimir Gorbenko <vacuum.go@gmail.com>
2019-10-16 11:52:35 +0200

    Updates

---
+++ precache-manifest.9836094a8f582f59376904c6625fa110.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b5bb2702703472e49bec8c1a2e02a28",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "ed4568a72b2f51cdca97",
+    "url": "/monsters/static/css/main.d95cc118.chunk.css"
+  },
+  {
+    "revision": "b0703eff4582a78f493a",
+    "url": "/monsters/static/js/2.5075ebff.chunk.js"
+  },
+  {
+    "revision": "ed4568a72b2f51cdca97",
+    "url": "/monsters/static/js/main.80b82910.chunk.js"
+  },
+  {
+    "revision": "b812b3a150035aea72a0",
+    "url": "/monsters/static/js/runtime-main.7c34276b.js"
+  }
+]);
